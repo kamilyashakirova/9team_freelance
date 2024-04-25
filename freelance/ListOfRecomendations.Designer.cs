8f2e111f5b6ba43e6d15b6eb27a911754968ca1a
kamilyashakirova <144135244+kamilyashakirova@users.noreply.github.com>
2024-04-25 14:19:16 +0300

    Add files via upload

---
+++ freelance/ListOfRecomendations.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace freelance.forms
+{
+    partial class ListOfRecomendations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListOfRecomendations));
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
+            settings_btn = new PictureBox();
+            likedlist_btn = new PictureBox();
+            like_btn = new PictureBox();
+            dislike_btn = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            updatedgv_pic = new PictureBox();
+            listofrecs_dgv1 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            pname_list = new DataGridViewTextBoxColumn();
+            pspecialisation_list = new DataGridViewTextBoxColumn();
+            ptime_list = new DataGridViewTextBoxColumn();
+            planguage_list = new DataGridViewTextBoxColumn();
+            pExperience_list = new DataGridViewTextBoxColumn();
+            pproduct_list = new DataGridViewTextBoxColumn();
+            rus_change_btn = new Button();
+            tat_change_btn = new Button();
+            ((System.ComponentModel.ISupportInitialize)settings_btn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)likedlist_btn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)like_btn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dislike_btn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)updatedgv_pic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)listofrecs_dgv1).BeginInit();
+            SuspendLayout();
+            // 
+            // settings_btn
+            // 
+            settings_btn.BackColor = Color.DarkSeaGreen;
+            settings_btn.Image = (Image)resources.GetObject("settings_btn.Image");
+            settings_btn.Location = new Point(9, 17);
+            settings_btn.Name = "settings_btn";
+            settings_btn.Size = new Size(85, 55);
+            settings_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            settings_btn.TabIndex = 1;
+            settings_btn.TabStop = false;
+            settings_btn.Click += settings_btn_Click;
+            settings_btn.MouseEnter += settings_btn_MouseEnter;
+            settings_btn.MouseLeave += settings_btn_MouseLeave;
+            // 
+            // likedlist_btn
+            // 
+            likedlist_btn.BackColor = Color.DarkSeaGreen;
+            likedlist_btn.Image = (Image)resources.GetObject("likedlist_btn.Image");
+            likedlist_btn.Location = new Point(96, 17);
+            likedlist_btn.Name = "likedlist_btn";
+            likedlist_btn.Size = new Size(85, 55);
+            likedlist_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            likedlist_btn.TabIndex = 2;
+            likedlist_btn.TabStop = false;
+            likedlist_btn.Click += likedlist_btn_Click;
+            likedlist_btn.MouseEnter += likedlist_btn_MouseEnter;
+            likedlist_btn.MouseLeave += likedlist_btn_MouseLeave;
+            // 
+            // like_btn
+            // 
+            like_btn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            like_btn.BackColor = Color.DarkSeaGreen;
+            like_btn.Image = (Image)resources.GetObject("like_btn.Image");
+            like_btn.Location = new Point(1007, 336);
+            like_btn.Name = "like_btn";
+            like_btn.Size = new Size(93, 68);
+            like_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            like_btn.TabIndex = 4;
+            like_btn.TabStop = false;
+            like_btn.Click += like_btn_Click;
+            like_btn.MouseEnter += like_btn_MouseEnter;
+            like_btn.MouseLeave += like_btn_MouseLeave;
+            // 
+            // dislike_btn
+            // 
+            dislike_btn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            dislike_btn.BackColor = Color.DarkSeaGreen;
+            dislike_btn.Image = (Image)resources.GetObject("dislike_btn.Image");
+            dislike_btn.Location = new Point(1007, 448);
+            dislike_btn.Name = "dislike_btn";
+            dislike_btn.Size = new Size(93, 75);
+            dislike_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            dislike_btn.TabIndex = 5;
+            dislike_btn.TabStop = false;
+            dislike_btn.Click += dislike_btn_Click;
+            dislike_btn.MouseEnter += dislike_btn_MouseEnter;
+            dislike_btn.MouseLeave += dislike_btn_MouseLeave;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Anchor = AnchorStyles.Left;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(18, 512);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(204, 151);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 22;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Anchor = AnchorStyles.Left;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(-121, 105);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(295, 282);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 23;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(891, -71);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(334, 296);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 24;
+            pictureBox6.TabStop = false;
+            // 
+            // updatedgv_pic
+            // 
+            updatedgv_pic.Image = (Image)resources.GetObject("updatedgv_pic.Image");
+            updatedgv_pic.Location = new Point(530, 12);
+            updatedgv_pic.Name = "updatedgv_pic";
+            updatedgv_pic.Size = new Size(89, 71);
+            updatedgv_pic.SizeMode = PictureBoxSizeMode.Zoom;
+            updatedgv_pic.TabIndex = 25;
+            updatedgv_pic.TabStop = false;
+            updatedgv_pic.Click += updatedgv_pic_Click;
+            // 
+            // listofrecs_dgv1
+            // 
+            listofrecs_dgv1.AllowUserToAddRows = false;
+            dataGridViewCellStyle6.Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle6.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = Color.DarkSeaGreen;
+            dataGridViewCellStyle6.SelectionForeColor = SystemColors.WindowText;
+            listofrecs_dgv1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            listofrecs_dgv1.BackgroundColor = SystemColors.Window;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = SystemColors.Control;
+            dataGridViewCellStyle7.Font = new Font("Didact Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle7.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = Color.DarkSeaGreen;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            listofrecs_dgv1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            listofrecs_dgv1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            listofrecs_dgv1.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, pname_list, pspecialisation_list, ptime_list, planguage_list, pExperience_list, pproduct_list });
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = SystemColors.Window;
+            dataGridViewCellStyle8.Font = new Font("Didact Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle8.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle8.SelectionBackColor = Color.DarkSeaGreen;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.False;
+            listofrecs_dgv1.DefaultCellStyle = dataGridViewCellStyle8;
+            listofrecs_dgv1.GridColor = Color.DarkSeaGreen;
+            listofrecs_dgv1.Location = new Point(180, 160);
+            listofrecs_dgv1.Name = "listofrecs_dgv1";
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = SystemColors.Control;
+            dataGridViewCellStyle9.Font = new Font("Didact Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle9.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = Color.DarkSeaGreen;
+            dataGridViewCellStyle9.SelectionForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.True;
+            listofrecs_dgv1.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            listofrecs_dgv1.RowHeadersWidth = 51;
+            dataGridViewCellStyle10.Font = new Font("Didact Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle10.ForeColor = Color.Black;
+            dataGridViewCellStyle10.SelectionBackColor = Color.DarkSeaGreen;
+            dataGridViewCellStyle10.SelectionForeColor = Color.Black;
+            listofrecs_dgv1.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            listofrecs_dgv1.Size = new Size(775, 552);
+            listofrecs_dgv1.TabIndex = 26;
+            listofrecs_dgv1.CellDoubleClick += listofrecs_dgv1_CellDoubleClick;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "ID";
+            dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            dataGridViewTextBoxColumn1.Visible = false;
+            dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // pname_list
+            // 
+            pname_list.HeaderText = "ФИО";
+            pname_list.MinimumWidth = 6;
+            pname_list.Name = "pname_list";
+            pname_list.Width = 125;
+            // 
+            // pspecialisation_list
+            // 
+            pspecialisation_list.HeaderText = "Специализация";
+            pspecialisation_list.MinimumWidth = 6;
+            pspecialisation_list.Name = "pspecialisation_list";
+            pspecialisation_list.Width = 125;
+            // 
+            // ptime_list
+            // 
+            ptime_list.HeaderText = "Срок";
+            ptime_list.MinimumWidth = 6;
+            ptime_list.Name = "ptime_list";
+            ptime_list.Width = 125;
+            // 
+            // planguage_list
+            // 
+            planguage_list.HeaderText = "Знание языков";
+            planguage_list.MinimumWidth = 6;
+            planguage_list.Name = "planguage_list";
+            planguage_list.Width = 125;
+            // 
+            // pExperience_list
+            // 
+            pExperience_list.HeaderText = "Опыт";
+            pExperience_list.MinimumWidth = 6;
+            pExperience_list.Name = "pExperience_list";
+            pExperience_list.Width = 125;
+            // 
+            // pproduct_list
+            // 
+            pproduct_list.HeaderText = "Продукт";
+            pproduct_list.MinimumWidth = 6;
+            pproduct_list.Name = "pproduct_list";
+            pproduct_list.Width = 125;
+            // 
+            // rus_change_btn
+            // 
+            rus_change_btn.BackColor = Color.LightCoral;
+            rus_change_btn.FlatStyle = FlatStyle.Flat;
+            rus_change_btn.Font = new Font("Didact Gothic", 13.8F);
+            rus_change_btn.Location = new Point(973, 12);
+            rus_change_btn.Name = "rus_change_btn";
+            rus_change_btn.Size = new Size(168, 60);
+            rus_change_btn.TabIndex = 27;
+            rus_change_btn.Text = "Русский";
+            rus_change_btn.UseVisualStyleBackColor = false;
+            rus_change_btn.Click += rus_change_btn_Click;
+            // 
+            // tat_change_btn
+            // 
+            tat_change_btn.BackColor = Color.LightCoral;
+            tat_change_btn.FlatStyle = FlatStyle.Flat;
+            tat_change_btn.Font = new Font("Didact Gothic", 12F);
+            tat_change_btn.Location = new Point(973, 12);
+            tat_change_btn.Name = "tat_change_btn";
+            tat_change_btn.Size = new Size(168, 55);
+            tat_change_btn.TabIndex = 28;
+            tat_change_btn.Text = "Татарча";
+            tat_change_btn.UseVisualStyleBackColor = false;
+            tat_change_btn.Visible = false;
+            tat_change_btn.Click += tat_change_btn_Click;
+            // 
+            // ListOfRecomendations
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
+            ClientSize = new Size(1153, 712);
+            Controls.Add(rus_change_btn);
+            Controls.Add(listofrecs_dgv1);
+            Controls.Add(updatedgv_pic);
+            Controls.Add(dislike_btn);
+            Controls.Add(like_btn);
+            Controls.Add(likedlist_btn);
+            Controls.Add(settings_btn);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(tat_change_btn);
+            Controls.Add(pictureBox6);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ListOfRecomendations";
+            Text = "Главная";
+            Load += listOfRecomendations_Load;
+            ((System.ComponentModel.ISupportInitialize)settings_btn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)likedlist_btn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)like_btn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dislike_btn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)updatedgv_pic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)listofrecs_dgv1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox settings_btn;
+        private PictureBox likedlist_btn;
+        private PictureBox like_btn;
+        private PictureBox dislike_btn;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn pname;
+        private DataGridViewTextBoxColumn pspecialization;
+        private DataGridViewTextBoxColumn ptime;
+        private DataGridViewTextBoxColumn planguage;
+        private DataGridViewTextBoxColumn pExperience;
+        private DataGridViewTextBoxColumn pproduct;
+        private PictureBox updatedgv_pic;
+        public DataGridView listofrecs_dgv1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn pname_list;
+        private DataGridViewTextBoxColumn pspecialisation_list;
+        private DataGridViewTextBoxColumn ptime_list;
+        private DataGridViewTextBoxColumn planguage_list;
+        private DataGridViewTextBoxColumn pExperience_list;
+        private DataGridViewTextBoxColumn pproduct_list;
+        private Button rus_change_btn;
+        private Button tat_change_btn;
+    }
+}
