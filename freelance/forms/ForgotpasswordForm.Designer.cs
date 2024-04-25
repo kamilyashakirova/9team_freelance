7e874f240a2c596f48bb93d44f487e79a4d3527e
kamilyashakirova <144135244+kamilyashakirova@users.noreply.github.com>
2024-04-25 15:25:07 +0300

    Add files via upload

---
+++ freelance/forms/ForgotpasswordForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace freelance.forms
+{
+    partial class ForgotpasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForgotpasswordForm));
+            newpassword_txt = new TextBox();
+            errorEXp_lbl = new Label();
+            errorRepeatnewp_lbl = new Label();
+            passwordrecovery_lbl = new Label();
+            writepassword_lbl = new Label();
+            login_txt = new TextBox();
+            loginerror_lbl = new Label();
+            passworderror_lbl = new Label();
+            login_lbl = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            sendpassword_btn = new Button();
+            newenter_btn = new Button();
+            exit_btn = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)exit_btn).BeginInit();
+            SuspendLayout();
+            // 
+            // newpassword_txt
+            // 
+            newpassword_txt.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            newpassword_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            newpassword_txt.Location = new Point(151, 301);
+            newpassword_txt.Name = "newpassword_txt";
+            newpassword_txt.PasswordChar = '*';
+            newpassword_txt.PlaceholderText = "Введите пароль";
+            newpassword_txt.Size = new Size(243, 30);
+            newpassword_txt.TabIndex = 2;
+            newpassword_txt.Visible = false;
+            // 
+            // errorEXp_lbl
+            // 
+            errorEXp_lbl.AutoSize = true;
+            errorEXp_lbl.Font = new Font("Didact Gothic", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            errorEXp_lbl.Location = new Point(148, 206);
+            errorEXp_lbl.Name = "errorEXp_lbl";
+            errorEXp_lbl.Size = new Size(0, 22);
+            errorEXp_lbl.TabIndex = 4;
+            // 
+            // errorRepeatnewp_lbl
+            // 
+            errorRepeatnewp_lbl.AutoSize = true;
+            errorRepeatnewp_lbl.Location = new Point(151, 376);
+            errorRepeatnewp_lbl.Name = "errorRepeatnewp_lbl";
+            errorRepeatnewp_lbl.Size = new Size(0, 20);
+            errorRepeatnewp_lbl.TabIndex = 5;
+            // 
+            // passwordrecovery_lbl
+            // 
+            passwordrecovery_lbl.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            passwordrecovery_lbl.AutoSize = true;
+            passwordrecovery_lbl.Font = new Font("Didact Gothic", 17.9999981F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            passwordrecovery_lbl.Location = new Point(102, 46);
+            passwordrecovery_lbl.Name = "passwordrecovery_lbl";
+            passwordrecovery_lbl.Size = new Size(343, 43);
+            passwordrecovery_lbl.TabIndex = 6;
+            passwordrecovery_lbl.Text = "Восстановление пароля";
+            // 
+            // writepassword_lbl
+            // 
+            writepassword_lbl.AutoSize = true;
+            writepassword_lbl.Font = new Font("Didact Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            writepassword_lbl.Location = new Point(49, 270);
+            writepassword_lbl.Name = "writepassword_lbl";
+            writepassword_lbl.Size = new Size(497, 28);
+            writepassword_lbl.TabIndex = 7;
+            writepassword_lbl.Text = "Введите пароль, который мы отправили на вашу почту";
+            writepassword_lbl.Visible = false;
+            // 
+            // login_txt
+            // 
+            login_txt.Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            login_txt.Location = new Point(102, 126);
+            login_txt.Name = "login_txt";
+            login_txt.PlaceholderText = "Введите логин";
+            login_txt.Size = new Size(352, 30);
+            login_txt.TabIndex = 8;
+            // 
+            // loginerror_lbl
+            // 
+            loginerror_lbl.AutoSize = true;
+            loginerror_lbl.Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            loginerror_lbl.Location = new Point(198, 156);
+            loginerror_lbl.Name = "loginerror_lbl";
+            loginerror_lbl.Size = new Size(135, 25);
+            loginerror_lbl.TabIndex = 9;
+            loginerror_lbl.Text = "Неверный логин";
+            loginerror_lbl.Visible = false;
+            // 
+            // passworderror_lbl
+            // 
+            passworderror_lbl.AutoSize = true;
+            passworderror_lbl.Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            passworderror_lbl.Location = new Point(198, 334);
+            passworderror_lbl.Name = "passworderror_lbl";
+            passworderror_lbl.Size = new Size(147, 25);
+            passworderror_lbl.TabIndex = 10;
+            passworderror_lbl.Text = "Неверный пароль";
+            passworderror_lbl.Visible = false;
+            // 
+            // login_lbl
+            // 
+            login_lbl.AutoSize = true;
+            login_lbl.Font = new Font("Didact Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            login_lbl.Location = new Point(198, 97);
+            login_lbl.Name = "login_lbl";
+            login_lbl.Size = new Size(142, 28);
+            login_lbl.TabIndex = 11;
+            login_lbl.Text = "Введите логин";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(424, -20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(204, 153);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(-62, 270);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(265, 335);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            // 
+            // sendpassword_btn
+            // 
+            sendpassword_btn.BackColor = SystemColors.Window;
+            sendpassword_btn.Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            sendpassword_btn.Location = new Point(127, 206);
+            sendpassword_btn.Name = "sendpassword_btn";
+            sendpassword_btn.Size = new Size(288, 36);
+            sendpassword_btn.TabIndex = 14;
+            sendpassword_btn.Text = "Отправить пароль на почту";
+            sendpassword_btn.UseVisualStyleBackColor = false;
+            sendpassword_btn.Click += sendpassword_btn_Click;
+            // 
+            // newenter_btn
+            // 
+            newenter_btn.BackColor = SystemColors.Window;
+            newenter_btn.Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            newenter_btn.Location = new Point(209, 361);
+            newenter_btn.Name = "newenter_btn";
+            newenter_btn.Size = new Size(111, 39);
+            newenter_btn.TabIndex = 15;
+            newenter_btn.Text = "Войти";
+            newenter_btn.UseVisualStyleBackColor = false;
+            newenter_btn.Visible = false;
+            newenter_btn.Click += newenter_btn_Click;
+            // 
+            // exit_btn
+            // 
+            exit_btn.Image = (Image)resources.GetObject("exit_btn.Image");
+            exit_btn.Location = new Point(2, 1);
+            exit_btn.Name = "exit_btn";
+            exit_btn.Size = new Size(52, 58);
+            exit_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            exit_btn.TabIndex = 30;
+            exit_btn.TabStop = false;
+            exit_btn.Click += exit_btn_Click;
+            // 
+            // ForgotpasswordForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
+            ClientSize = new Size(558, 412);
+            Controls.Add(exit_btn);
+            Controls.Add(newenter_btn);
+            Controls.Add(sendpassword_btn);
+            Controls.Add(login_lbl);
+            Controls.Add(passworderror_lbl);
+            Controls.Add(loginerror_lbl);
+            Controls.Add(login_txt);
+            Controls.Add(writepassword_lbl);
+            Controls.Add(passwordrecovery_lbl);
+            Controls.Add(errorRepeatnewp_lbl);
+            Controls.Add(errorEXp_lbl);
+            Controls.Add(newpassword_txt);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ForgotpasswordForm";
+            Text = "восстановление пароля";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)exit_btn).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox newpassword_txt;
+        private Label errorEXp_lbl;
+        private Label errorRepeatnewp_lbl;
+        private Label passwordrecovery_lbl;
+        private Label writepassword_lbl;
+        private TextBox login_txt;
+        private Label loginerror_lbl;
+        private Label passworderror_lbl;
+        private Label login_lbl;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button sendpassword_btn;
+        private Button newenter_btn;
+        private PictureBox exit_btn;
+    }
+}
---
+++ freelance/forms/PerformerCard.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace freelance.forms
+{
+    partial class PerformerCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PerformerCard));
+            ppictureCard_pic = new PictureBox();
+            pnameCard_txt = new TextBox();
+            pspecializationCard_txt = new TextBox();
+            ptimeCard_txt = new TextBox();
+            planguage_txt = new TextBox();
+            pproduct_txt = new TextBox();
+            pExpipienceCard_txt = new TextBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            fio_lbl = new Label();
+            spec_lbl = new Label();
+            srtime_lbl = new Label();
+            srprice_lbl = new Label();
+            experiense_lbl = new Label();
+            rating_lbl = new Label();
+            like_btn = new PictureBox();
+            dislike_btn = new PictureBox();
+            ID_Card_txt = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)ppictureCard_pic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)like_btn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dislike_btn).BeginInit();
+            SuspendLayout();
+            // 
+            // ppictureCard_pic
+            // 
+            ppictureCard_pic.BorderStyle = BorderStyle.FixedSingle;
+            ppictureCard_pic.Location = new Point(14, 36);
+            ppictureCard_pic.Margin = new Padding(3, 4, 3, 4);
+            ppictureCard_pic.Name = "ppictureCard_pic";
+            ppictureCard_pic.Size = new Size(287, 347);
+            ppictureCard_pic.SizeMode = PictureBoxSizeMode.Zoom;
+            ppictureCard_pic.TabIndex = 0;
+            ppictureCard_pic.TabStop = false;
+            // 
+            // pnameCard_txt
+            // 
+            pnameCard_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            pnameCard_txt.Location = new Point(335, 57);
+            pnameCard_txt.Margin = new Padding(3, 4, 3, 4);
+            pnameCard_txt.Name = "pnameCard_txt";
+            pnameCard_txt.Size = new Size(580, 30);
+            pnameCard_txt.TabIndex = 1;
+            // 
+            // pspecializationCard_txt
+            // 
+            pspecializationCard_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            pspecializationCard_txt.Location = new Point(335, 136);
+            pspecializationCard_txt.Margin = new Padding(3, 4, 3, 4);
+            pspecializationCard_txt.Name = "pspecializationCard_txt";
+            pspecializationCard_txt.Size = new Size(580, 30);
+            pspecializationCard_txt.TabIndex = 2;
+            // 
+            // ptimeCard_txt
+            // 
+            ptimeCard_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            ptimeCard_txt.Location = new Point(335, 222);
+            ptimeCard_txt.Margin = new Padding(3, 4, 3, 4);
+            ptimeCard_txt.Name = "ptimeCard_txt";
+            ptimeCard_txt.Size = new Size(304, 30);
+            ptimeCard_txt.TabIndex = 3;
+            // 
+            // planguage_txt
+            // 
+            planguage_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            planguage_txt.Location = new Point(335, 315);
+            planguage_txt.Margin = new Padding(3, 4, 3, 4);
+            planguage_txt.Name = "planguage_txt";
+            planguage_txt.Size = new Size(194, 30);
+            planguage_txt.TabIndex = 4;
+            // 
+            // pproduct_txt
+            // 
+            pproduct_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            pproduct_txt.Location = new Point(335, 490);
+            pproduct_txt.Margin = new Padding(3, 4, 3, 4);
+            pproduct_txt.Name = "pproduct_txt";
+            pproduct_txt.Size = new Size(177, 30);
+            pproduct_txt.TabIndex = 5;
+            // 
+            // pExpipienceCard_txt
+            // 
+            pExpipienceCard_txt.Font = new Font("Didact Gothic", 10.1999989F);
+            pExpipienceCard_txt.Location = new Point(335, 401);
+            pExpipienceCard_txt.Margin = new Padding(3, 4, 3, 4);
+            pExpipienceCard_txt.Name = "pExpipienceCard_txt";
+            pExpipienceCard_txt.Size = new Size(177, 30);
+            pExpipienceCard_txt.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(674, 362);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(467, 344);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(160, 474);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(210, 178);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(804, 206);
+            pictureBox3.Margin = new Padding(3, 4, 3, 4);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(96, 105);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // fio_lbl
+            // 
+            fio_lbl.AutoSize = true;
+            fio_lbl.Font = new Font("Didact Gothic", 10.1999989F);
+            fio_lbl.Location = new Point(335, 28);
+            fio_lbl.Name = "fio_lbl";
+            fio_lbl.Size = new Size(52, 25);
+            fio_lbl.TabIndex = 10;
+            fio_lbl.Text = "ФИО:";
+            // 
+            // spec_lbl
+            // 
+            spec_lbl.AutoSize = true;
+            spec_lbl.Font = new Font("Didact Gothic", 10.1999989F);
+            spec_lbl.Location = new Point(335, 107);
+            spec_lbl.Name = "spec_lbl";
+            spec_lbl.Size = new Size(133, 25);
+            spec_lbl.TabIndex = 11;
+            spec_lbl.Text = "Специализация:";
+            // 
+            // srtime_lbl
+            // 
+            srtime_lbl.AutoSize = true;
+            srtime_lbl.Font = new Font("Didact Gothic", 10.1999989F);
+            srtime_lbl.Location = new Point(335, 193);
+            srtime_lbl.Name = "srtime_lbl";
+            srtime_lbl.Size = new Size(286, 25);
+            srtime_lbl.TabIndex = 12;
+            srtime_lbl.Text = "Среднее время выполнения работы:";
+            // 
+            // srprice_lbl
+            // 
+            srprice_lbl.AutoSize = true;
+            srprice_lbl.Font = new Font("Didact Gothic", 10.1999989F);
+            srprice_lbl.Location = new Point(335, 286);
+            srprice_lbl.Name = "srprice_lbl";
+            srprice_lbl.Size = new Size(126, 25);
+            srprice_lbl.TabIndex = 13;
+            srprice_lbl.Text = "Знание языков:";
+            // 
+            // experiense_lbl
+            // 
+            experiense_lbl.AutoSize = true;
+            experiense_lbl.Font = new Font("Didact Gothic", 10.1999989F);
+            experiense_lbl.Location = new Point(335, 372);
+            experiense_lbl.Name = "experiense_lbl";
+            experiense_lbl.Size = new Size(56, 25);
+            experiense_lbl.TabIndex = 14;
+            experiense_lbl.Text = "Опыт:";
+            // 
+            // rating_lbl
+            // 
+            rating_lbl.AutoSize = true;
+            rating_lbl.Font = new Font("Didact Gothic", 10.1999989F);
+            rating_lbl.Location = new Point(335, 461);
+            rating_lbl.Name = "rating_lbl";
+            rating_lbl.Size = new Size(79, 25);
+            rating_lbl.TabIndex = 15;
+            rating_lbl.Text = "Продукт:";
+            // 
+            // like_btn
+            // 
+            like_btn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            like_btn.BackColor = Color.DarkSeaGreen;
+            like_btn.BorderStyle = BorderStyle.FixedSingle;
+            like_btn.Image = (Image)resources.GetObject("like_btn.Image");
+            like_btn.Location = new Point(74, 391);
+            like_btn.Margin = new Padding(3, 4, 3, 4);
+            like_btn.Name = "like_btn";
+            like_btn.Size = new Size(72, 53);
+            like_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            like_btn.TabIndex = 16;
+            like_btn.TabStop = false;
+            like_btn.Click += like_btn_Click;
+            like_btn.MouseEnter += like_btn_MouseEnter;
+            like_btn.MouseLeave += like_btn_MouseLeave;
+            // 
+            // dislike_btn
+            // 
+            dislike_btn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dislike_btn.BackColor = Color.DarkSeaGreen;
+            dislike_btn.BorderStyle = BorderStyle.FixedSingle;
+            dislike_btn.Image = (Image)resources.GetObject("dislike_btn.Image");
+            dislike_btn.Location = new Point(163, 391);
+            dislike_btn.Margin = new Padding(3, 4, 3, 4);
+            dislike_btn.Name = "dislike_btn";
+            dislike_btn.Size = new Size(72, 53);
+            dislike_btn.SizeMode = PictureBoxSizeMode.Zoom;
+            dislike_btn.TabIndex = 17;
+            dislike_btn.TabStop = false;
+            dislike_btn.Click += dislike_btn_Click;
+            dislike_btn.MouseEnter += dislike_btn_MouseEnter;
+            dislike_btn.MouseLeave += dislike_btn_MouseLeave;
+            // 
+            // ID_Card_txt
+            // 
+            ID_Card_txt.Location = new Point(48, 515);
+            ID_Card_txt.Name = "ID_Card_txt";
+            ID_Card_txt.Size = new Size(125, 30);
+            ID_Card_txt.TabIndex = 18;
+            ID_Card_txt.Visible = false;
+            // 
+            // PerformerCard
+            // 
+            AutoScaleDimensions = new SizeF(9F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
+            ClientSize = new Size(970, 610);
+            Controls.Add(ID_Card_txt);
+            Controls.Add(dislike_btn);
+            Controls.Add(like_btn);
+            Controls.Add(rating_lbl);
+            Controls.Add(experiense_lbl);
+            Controls.Add(srprice_lbl);
+            Controls.Add(srtime_lbl);
+            Controls.Add(spec_lbl);
+            Controls.Add(fio_lbl);
+            Controls.Add(pExpipienceCard_txt);
+            Controls.Add(pproduct_txt);
+            Controls.Add(planguage_txt);
+            Controls.Add(ptimeCard_txt);
+            Controls.Add(pspecializationCard_txt);
+            Controls.Add(pnameCard_txt);
+            Controls.Add(ppictureCard_pic);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox1);
+            Font = new Font("Didact Gothic", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "PerformerCard";
+            Text = "Карточка фрилансера";
+            ((System.ComponentModel.ISupportInitialize)ppictureCard_pic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)like_btn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dislike_btn).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        public PictureBox ppictureCard_pic;
+        public TextBox pnameCard_txt;
+        public TextBox pspecializationCard_txt;
+        public TextBox ptimeCard_txt;
+        public TextBox planguage_txt;
+        public TextBox pproduct_txt;
+        public TextBox pExpipienceCard_txt;
+        public Label fio_lbl;
+        public Label spec_lbl;
+        public Label srtime_lbl;
+        public Label srprice_lbl;
+        public Label experiense_lbl;
+        public Label rating_lbl;
+        public PictureBox like_btn;
+        public PictureBox dislike_btn;
+        public TextBox ID_Card_txt;
+    }
+}
