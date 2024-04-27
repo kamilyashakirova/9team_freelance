0f3afb82f46f46b37468673d6d61c7404bea4813
kamilyashakirova <ksakirova2005@mail.ru>
2024-04-27 22:54:58 +0300

    добавлены Unit-тесты

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,16 +1,13 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
-namespace freelance.Tests
+namespace freelance
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests
     {
         [TestMethod]
         public void CheckLocalisated()
         {
-            string key = "hello";
-            var result = "Bonjour";
+            string key = "key";
+            var result = "value";
             var res = ForUnitTests.CheckLocalization(key);
             Assert.AreEqual(result, res);
         }
@@ -26,5 +23,89 @@ namespace freelance.Tests
             ForUnitTests.CheckSendEmail(validEmailAddress, message);
             ForUnitTests.CheckSendEmail(invalidEmailAddress, message);
         }
+
+        [TestMethod]
+        public void TestSurnameTextChange() //имя
+        {
+            // Arrange
+            string message1 = "Поле имени не должно быть пустым";
+            string message2 = "Имя не должно содержать цифры";
+
+            //Act
+            ForUnitTests.Test_Surname_TextChange(message1, message2);
+        }
+
+        [TestMethod]
+        public void TestPatronomicTextChanged() //фамилия
+        {
+            // Arrange
+            string message1 = "Поле фамилии не должно быть пустым";
+            string message2 = "Фамилия не должна содержать цифры";
+
+            //Act
+            ForUnitTests.Test_Patronomic_TextChanged(message1, message2);
+        }
+
+        [TestMethod]
+        public void TestRloginTextChange() //отчество
+        {
+            //Arrange
+            string message1 = "Отчество не должно содержать цифры";
+
+            //Act 
+            ForUnitTests.Test_Rlogin_TextChange(message1);
+        }
+
+        [TestMethod]
+        public void Test_REmail_TextChange()
+        {
+            //Arrange
+            string message1 = "Поле логин не должно быть пустым";
+            string message2 = "Поле пароль не должно быть пустым";
+
+            //Act
+            ForUnitTests.Test_REmail_TextChange(message1, message2);
+        }
+
+        [TestMethod]
+        public void TestCheckDislikedPerformers()
+        {
+            //Arrange
+            string message1 = "Вы уже добавляли фрилансера в скрытое";
+
+            //Act
+            ForUnitTests.TestCheckDislikedPerformers(message1);
+        }
+
+        [TestMethod]
+        public void TestAddDislikeToPerformer()
+        {
+            //Arrange
+            string message1 = "Фрилансер успешно добавлен в 'Скрытое'";
+
+            //Act
+            ForUnitTests.TestAddDislikeToPerformer(message1);
+        }
+
+        [TestMethod]
+        public void TestAddLikeIfPerformerNotLiked()
+        {
+            //Arrange
+            string message1 = "Вы уже добавляли фрилансера в 'Избранное'";
+
+            //Act
+            ForUnitTests.TestAddLikedPerformers(message1);
+        }
+
+        [TestMethod]
+        public void TestAddLikedPerformers()
+        {
+            //Arrange
+            string message1 = "Фрилансер успешно добавлен в 'Избранное'";
+
+            //Act
+            ForUnitTests.TestAddDislikeToPerformer(message1);
+        }
     }
+
 }
--- freelance/ForUnitTests.cs
+++ freelance/ForUnitTests.cs
@@ -1,7 +1,5 @@
 ﻿using freelance.forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Net.Mail;
-using System.ComponentModel;
 namespace freelance
 {
     public static class ForUnitTests
@@ -10,9 +8,9 @@ namespace freelance
         {
 
             Localization.localizationDictionary = new Dictionary<string, string>()
-            {
-                { a, "Bonjour" }
-            };
+        {
+            { a, "value" }
+        };
             var res = Localization.GetLocalizedString(a);
             return res;
         }
@@ -43,5 +41,120 @@ namespace freelance
                 MessageBox.Show(ex.ToString());
             }
         }
+
+        public static void Test_Surname_TextChange(string message1, string message2)
+        {
+            var textBox = new TextBox();
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                MessageBox.Show(message1);
+            }
+            if (textBox.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show(message2);
+            }
+        }
+
+        public static void Test_Patronomic_TextChanged(string message1, string message2)
+        {
+            var textBox = new TextBox();
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                MessageBox.Show(message1);
+            }
+            if (textBox.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show(message2);
+            }
+        }
+
+        public static void Test_Rlogin_TextChange(string message1)
+        {
+            var textBox = new TextBox();
+
+            if (textBox.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show(message1);
+            }
+        }
+
+        public static void Test_REmail_TextChange(string message1, string message2)
+        {
+            var textBox = new TextBox();
+            var label = new Label();
+
+            if (textBox.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show(message1);
+            }
+            if (label.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show(message2);
+            }
+        }
+
+        public static void TestCheckDislikedPerformers(string message1)
+        {
+            using (var db = new DBcontext())
+            {
+                var performer = db.Performers.Where(p => p.ID.ToString() == "1").FirstOrDefault();
+                if (performer != null)
+                {
+                    if (db.DislikedPerformers.Any(u => u.PerformerID == performer.ID))
+                    {
+                        MessageBox.Show(message1);
+                    }
+                }
+            }
+
+        }
+
+        public static void TestAddLikeIfPerformerNotLiked(string message1)
+        {
+            using (var db = new DBcontext())
+            {
+                var performer = db.Performers.Where(p => p.ID.ToString() == "2").FirstOrDefault();
+                if (performer != null)
+                    if (db.LikedPerformers.Any(u => u.PerformerID == performer.ID))
+                    {
+                        MessageBox.Show(message1);
+                    }
+            }
+        }
+
+        public static void TestAddLikedPerformers(string message1)
+        {
+            using (var db = new DBcontext())
+            {
+                var performer = db.Performers.Where(p => p.ID.ToString() == "2").FirstOrDefault();
+                int clientID = 1;
+                if (performer != null)
+                    if (!db.LikedPerformers.Any(u => u.PerformerID == performer.ID))
+                    {
+                        workingwithDB.AddLike(clientID, performer.ID);
+                    }
+                    else
+                    {
+                        MessageBox.Show(message1);
+                    }
+            }
+        }
+
+        public static void TestAddDislikeToPerformer(string message1)
+        {
+            using (var db = new DBcontext())
+            {
+                var performer = db.Performers.Where(p => p.ID.ToString() == "1").FirstOrDefault();
+                if (performer != null)
+                    if (!db.DislikedPerformers.Any(u => u.PerformerID == performer.ID))
+                    {
+                        workingwithDB.AddDislike(performer.ClientID, performer.ID);
+                        MessageBox.Show(message1);
+                    }
+            }
+        }
     }
+
 }
